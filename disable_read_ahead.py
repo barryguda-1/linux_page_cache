d7d9548097892798d42aaf24bc4fbade3e7ba161
Barry Guda <52743757+barryguda-1@users.noreply.github.com>
2023-04-13 08:28:35 +0200

    Update disable_read_ahead.py

--- disable_read_ahead.py
+++ disable_read_ahead.py
@@ -3,4 +3,5 @@ import os
 with open("/var/tmp/file1.db", "br") as f:  
     fd = f.fileno()  
     os.posix_fadvise(fd, 0, os.fstat(fd).st_size, os.POSIX_FADV_RANDOM)  
+    #read 2 bytes of data
     print(f.read(2))
